349eae490c53b790dc487cfd5aacb4fe33942dcb
Michael Shimeles <michaelwasihun96@gmail.com>
2025-05-02 11:14:45 -0400

    fix notification bug

--- app/dashboard/notifications/page.tsx
+++ app/dashboard/notifications/page.tsx
@@ -1,9 +1,12 @@
 import { db } from "@/db/drizzle";
 import { passes } from "@/db/schema";
 import NotificationsForm from "./form";
+import { eq } from "drizzle-orm";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function NotificationsPage() {
-  const allPasses = await db.select().from(passes);
+  const { userId } = await auth();
+  const allPasses = await db.select().from(passes).where(eq(passes.userId, userId!));
 
   return (
     <div className="p-6 max-w-xl space-y-4">
