58d50246e7d4dace6a47958d25917fdc1601095e
Michael Shimeles <michaelwasihun96@gmail.com>
2025-05-21 19:04:15 -0700

    staging

--- app/dashboard/image/page.tsx
+++ app/dashboard/image/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
 import { useState } from "react";
